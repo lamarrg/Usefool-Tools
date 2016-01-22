51f71df35e6b10ec1790eee9f6d7199328cc7fa1
Lamar <github@lamargreene.com>
2016-01-21 20:08:59 -0600

    Initial Commit

---
+++ Usefool Tools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Usefool Tools
+//
+//  Created by Lamar Greene on 1/21/16.
+//  Copyright © 2016 Lamar Greene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
